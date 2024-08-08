1cadd6db7606950653d73cfe0f00b65d33deb474
Will Adler <will@wtadler.com>
2024-08-08 14:30:23 -0400

    await async_forward_entry_setups
    addresses deprecation warning about calling async_forward_entry_setup

--- custom_components/ical/__init__.py
+++ custom_components/ical/__init__.py
@@ -49,10 +49,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.data[DOMAIN] = {}
     hass.data[DOMAIN][config.get(CONF_NAME)] = ICalEvents(hass=hass, config=config)
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
